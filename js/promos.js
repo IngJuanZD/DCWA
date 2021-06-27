f7a64ca7e41d38dff595950251412fa2308ec0d8
IngJuanZD <ing.juancarloszd@gmail.com>
2021-06-27 03:12:52 -0500

    actualizacion del precio de DCW0001

--- js/promos.js
+++ js/promos.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   //console.log('dentro funcion');  //solo visula
 
   const xhttp = new XMLHttpRequest();
-  xhttp.open("GET", "https://catalogo-dcw.firebaseio.com/.json", true);
+  xhttp.open('GET', 'json/catalogo-dcw.json', true);
   xhttp.send();
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
